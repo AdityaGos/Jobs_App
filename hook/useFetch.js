7945eb023dc493f8faba0dd9ccae6cb0e5aa5a06
Aditya Goswami <adityagoswami071@gmail.com>
2023-06-11 00:43:01 +0530

    feat: add useFetch method (API endpoint)

---
+++ hook/useFetch.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const options = {
+        method: "GET",
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+
+        // params: {
+        //   query: 'Python developer in Texas, USA',
+        //   page: '1',
+        //   num_pages: '1'
+        // },
+        params: {
+            ...query
+        },
+        headers: {
+            "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+            "X-RapidAPI-Host": "jsearch.p.rapidapi.com"
+        }
+    }
+
+    const fetchData = async () => {
+        setIsLoading(true)
+        try {
+            const response = await axios.request(options)
+            setData(response.data.data)
+            setIsLoading(false)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const refetch = () => {
+        setIsLoading(true)
+        fetchData()
+    }
+    return { data, isLoading, error, refetch }
+}
+export default useFetch
